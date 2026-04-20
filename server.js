be5e078b8ab328e8ec971c60406156d5b0d6a80c
sharesellqt-lang <sharesellqt@gmail.com>
2026-04-20 13:01:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,26 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// fake database key
+const VALID_KEYS = ["PRO123", "VIP999", "ADMIN"];
+
 app.get("/", (req, res) => {
   res.send("License Server Running 🚀");
 });
 
 app.get("/healthz", (req, res) => {
   res.send("OK");
+});
+
+// 🔐 API CHECK KEY
+app.get("/verify", (req, res) => {
+  const key = req.query.key;
+
+  if (VALID_KEYS.includes(key)) {
+    return res.json({ valid: true });
+  }
+
+  return res.json({ valid: false });
 });
 
 app.listen(PORT, () => {
