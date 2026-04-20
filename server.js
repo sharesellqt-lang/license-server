60edd435fd2cde7747bae64046e55a1527dd7716
sharesellqt-lang <sharesellqt@gmail.com>
2026-04-20 12:49:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("server running");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("License Server Running 🚀");
+});
+
+app.get("/healthz", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
