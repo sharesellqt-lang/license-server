91688e741420ca291126d70f51fdc9ad835ca349
sharesellqt-lang <sharesellqt@gmail.com>
2026-04-27 21:27:30 +0700

    fix mongo auth

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 // =========================
 const PORT = process.env.PORT || 10000;
 
-const MONGO_URI = "mongodb+srv://sharesellqt_db_user:1RMEJMvtsQvDL4pL@license-cluster.y92xgoq.mongodb.net/?appName=license-cluster";
+const MONGO_URI = "mongodb://sharesellqt_db_user:1RMEJMvtsQvDL4pL@license-cluster-shard-00-00.y92xgoq.mongodb.net:27017,license-cluster-shard-00-01.y92xgoq.mongodb.net:27017,license-cluster-shard-00-02.y92xgoq.mongodb.net:27017/?ssl=true&replicaSet=atlas-0&authSource=admin";
 
 const WP_API = "https://sharesell.net/wp-json/wp/v2/posts";
 
