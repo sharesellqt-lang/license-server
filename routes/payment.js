ad812f822f31bd86a1844a052bc7abe6c4cb4ea8
sharesellqt-lang <sharesellqt@gmail.com>
2026-05-10 08:34:58 +0700

    fix payment

--- routes/payment.js
+++ routes/payment.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const db = require("../db");
 
-const PLANS = require("../config/plans");
+const PLANS = require("../routes/plans");
 
 // =====================================================
 // CREATE PAYMENT
