bb2727518500d6300c86810a6dbf701ae06bae18
sharesellqt-lang <sharesellqt@gmail.com>
2026-05-12 12:35:42 +0700

    fix login

--- routes/admin.js
+++ routes/admin.js
@@ -53,7 +53,7 @@ module.exports = router;
 // =====================================
 // ADMIN AUTH
 // =====================================
-const jwt = require("jsonwebtoken");
+
 const SECRET = process.env.JWT_SECRET || "supersecret";
 
 function adminAuth(req, res, next) {
