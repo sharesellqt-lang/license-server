cdb31714e25f1ecb9e54643fb7884726cff2b366
sharesellqt-lang <sharesellqt@gmail.com>
2026-05-20 21:05:12 +0700

    payment

--- server.js
+++ server.js
@@ -27,8 +27,12 @@ app.use(cors({
     "https://sharesell.net",
     "http://localhost:3000"
   ],
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.options("*", cors());
 
 // 🔥 parse json trước
 app.use(express.json());
