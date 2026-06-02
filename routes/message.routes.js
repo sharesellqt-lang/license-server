56e94b5051fc84267da05b1534ee26095d219236
sharesellqt-lang <sharesellqt@gmail.com>
2026-06-02 13:41:48 +0700

    update backend

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const ctrl = require("../controllers/message.controller");
 const auth = require("../middleware/auth");
 
-router.post("/", auth.verifyUser, ctrl.sendMessage);
+router.post("/", auth, ctrl.sendMessage);
 router.get("/:match_id", auth, ctrl.getMessages);
 
 module.exports = router;
