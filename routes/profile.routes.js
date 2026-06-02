f9dd270ef357d563a94908387aa8ab59c85bfd8b
sharesellqt-lang <sharesellqt@gmail.com>
2026-06-02 13:23:13 +0700

    update backend

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const ctrl = require("../controllers/profile.controller");
 const auth = require("../middleware/auth");
 
-router.get("/", auth.verifyUser, ctrl.getProfiles);
+router.get("/", auth, ctrl.getProfiles);
 
 module.exports = router;
