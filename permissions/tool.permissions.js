c0e6d1bc0ec3df452118a47c107883ed99100d56
sharesellqt-lang <sharesellqt@gmail.com>
2026-06-18 16:26:36 +0700

    update backend

--- permissions/tool.permissions.js
+++ permissions/tool.permissions.js
@@ -3,19 +3,62 @@ module.exports = {
   features: {
 
     proMode: {
-
       requiredPlan: "pro",
-
       trialDays: 3
-
     },
 
     vipMode: {
-
       requiredPlan: "vip",
-
       trialDays: 1
+    },
 
+    // Airdrop Hunter PRO
+
+    galxeScanner: {
+      requiredPlan: "pro",
+      trialDays: 3
+    },
+
+    zealyScanner: {
+      requiredPlan: "pro",
+      trialDays: 3
+    },
+
+    layer3Scanner: {
+      requiredPlan: "pro",
+      trialDays: 3
+    },
+
+    questnScanner: {
+      requiredPlan: "pro",
+      trialDays: 3
+    },
+
+    csvExport: {
+      requiredPlan: "pro",
+      trialDays: 3
+    },
+
+    telegramAlert: {
+      requiredPlan: "pro",
+      trialDays: 3
+    },
+
+    // Airdrop Hunter VIP
+
+    aiScore: {
+      requiredPlan: "vip",
+      trialDays: 1
+    },
+
+    smartRanking: {
+      requiredPlan: "vip",
+      trialDays: 1
+    },
+
+    walletTracking: {
+      requiredPlan: "vip",
+      trialDays: 1
     }
 
   }
