716b0aa625b7e9c3c5718a74dac75490dd37e100
sharesellqt-lang <sharesellqt@gmail.com>
2026-08-01 14:56:56 +0700

    fix backend'

--- services/airdrop.metrics.service.js
+++ services/airdrop.metrics.service.js
@@ -1338,13 +1338,6 @@ WHERE
 
     ];
 
-    console.log("====== SAVE METRICS DATA ======");
-console.dir(data, { depth: null });
-
-const normalized = normalizeMetrics(data);
-
-console.log("====== NORMALIZED ======");
-console.dir(normalized, { depth: null });
     const [result] =
         await db.query(
             sql,
